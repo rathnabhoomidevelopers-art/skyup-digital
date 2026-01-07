66a0bc53498d5a91edc45a322b5647589304bae2
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-01-07 16:49:23 +0530

    updated the canonical tags

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -80,6 +80,11 @@ export function Careers() {
           name="description"
           content="Discover exciting Digital Marketing Jobs offering structured learning, career growth, and hands-on industry experience to boost your professional journey."
         />
+        <meta
+          name="keywords"
+          content="Digital marketing Jobs"
+        />
+        <link rel="canonical" href="https://www.skyupdigitalsolutions.com/careers" />
       </Helmet>
       <Header />
 
