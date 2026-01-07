99ed08b2bfa72e767b9cd3f2cef9b67e82e41558
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-01-07 16:49:23 +0530

    updated the canonical tags

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -45,12 +45,17 @@ export function Blogs(){
     return(
         <div className="font-poppins">
           <Helmet>
-                  <title>Digital Marketing vs Traditional Marketing: What You Should Know</title>
-                  <meta
-                    name="description"
-                    content="Discover the difference between digital marketing and traditional marketing to enhance strategies and drive better business results."
-                  />
-                </Helmet>
+            <title>Digital Marketing vs Traditional Marketing: What You Should Know</title>
+            <meta
+              name="description"
+              content="Discover the difference between digital marketing and traditional marketing to enhance strategies and drive better business results."
+            />
+            <meta
+              name="keywords"
+              content="Difference between digital marketing and traditional marketing"
+            />
+            <link rel="canonical" href="https://www.skyupdigitalsolutions.com/blogs" />
+          </Helmet>
             <Header/>
             <motion.div
                     initial="hidden"
--- src/pages/Service.js
+++ src/pages/Service.js
@@ -48,6 +48,11 @@ export function Service() {
           name="description"
           content="End-to-end Digital Marketing Services in Bangalore to boost visibility, generate quality leads, and achieve sustainable business growth."
         />
+        <meta
+          name="keywords"
+          content="Digital Marketing Services in Bangalore"
+        />
+        <link rel="canonical" href="https://www.skyupdigitalsolutions.com/service" />
       </Helmet>
       <Header />
 
