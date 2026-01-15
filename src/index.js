20867bbf66585562549894aa703fd4fe72fa430b
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-01-15 15:03:21 +0530

    removed all the updates

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-import React from 'react';
+import { HelmetProvider } from "react-helmet-async";
 import ReactDOM from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
-import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import App from './App';
 
-// Import pre-render setup file (we'll create this in the next step)
-import './setupPreRender';
-
-const rootElement = document.getElementById('root');
-
-// Check if HTML was pre-rendered by react-snap
-if (rootElement.hasChildNodes()) {
-  // Hydrate the pre-rendered HTML - this is the React 18 way
-  ReactDOM.hydrateRoot(
-    rootElement,
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  );
-} else {
-  // Normal render for development when there's no pre-rendered HTML
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
