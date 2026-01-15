6d431d133f8ef1825436eab2592e7a8c29125750
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-01-15 16:47:58 +0530

    updated the noscript tag

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 // Import pre-render setup file (we'll create this in the next step)
 import './setupPreRender';
 
