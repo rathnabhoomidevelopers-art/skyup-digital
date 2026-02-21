547458ae5794dbe4e11899cc4ced963d87885f0c
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-02-21 12:25:12 +0530

    removed the duplicate keywords and descriptions

--- pages/contactus/+Head.jsx
+++ pages/contactus/+Head.jsx
@@ -1,7 +1,6 @@
 export default function Head() {
   return (
     <>
-      <meta name="keywords" content="Contact us" />
       <link
         rel="canonical"
         href="https://www.skyupdigitalsolutions.com/contactus"
