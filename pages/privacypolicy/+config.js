5594c96af10d025a20c233b67a8cfd1ac1208707
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-02-21 14:55:04 +0530

    Added the meta title and description for the terms and conditions and privacy policy

---
+++ pages/privacypolicy/+config.js
@@ -0,0 +1,5 @@
+// pages/privacypolicy/+config.js
+export default {
+  title: 'Privacy & Data Policy – Skyup Digital Solutions',
+  metaDescription: 'Learn how Skyup Digital Solutions collects, uses, and protects your personal information while ensuring data security, transparency, and privacy compliance.'
+}
---
+++ pages/termscondition/+config.js
@@ -0,0 +1,5 @@
+// pages/termscondition/+config.js
+export default {
+  title: 'Skyup Digital Solutions Terms & Conditions',
+  metaDescription: 'Read the Terms & Conditions of Skyup Digital Solutions outlining service usage, responsibilities, policies, and legal guidelines for users.'
+}
