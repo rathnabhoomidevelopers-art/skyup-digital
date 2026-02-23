dd155d8010b330abf2f00b4bc7ae556b4b38ec78
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-02-23 16:26:27 +0530

    updated the login page

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,2 @@
-// src/config/api.js
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3500';
-
+const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3500';
 export default API_URL;
