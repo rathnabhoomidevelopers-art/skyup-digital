e30277a90933b54fcb8b9163546a52f4291f1473
rathnabhoomidevelopers-art <rathnabhoomidevelopers@gmail.com>
2026-02-28 15:53:55 +0530

    updated the view more button for FAQ's

--- pages/index/+Page.jsx
+++ pages/index/+Page.jsx
@@ -2,8 +2,6 @@ import '../../src/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { HomePage } from '../../src/pages/HomePage'
 
-
-
 export default function Page() {
   return <HomePage />;
 }
