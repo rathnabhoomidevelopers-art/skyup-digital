ab4ab48bd87bb10854a86abe99049fb9825e8c90
Pooja <poojakadwadi8888@gmail.com>
2026-03-28 10:02:22 +0530

    Update +config.js

--- pages/aboutus-digital-marketing-company-in-bangalore/+config.js
+++ pages/aboutus-digital-marketing-company-in-bangalore/+config.js
@@ -1,5 +1,5 @@
 export default {
     title: 'Top Digital Marketing Company in Bangalore | SKYUP' ,
     metaDescription:"SkyUp is a results-driven Digital Marketing Company in Bangalore offering strategic marketing solutions to boost visibility, leads, and growth.",
-    keywords: "Dgital Marketing Company in Bangalore"
+    keywords: "Digital Marketing Company in Bangalore"
 }
