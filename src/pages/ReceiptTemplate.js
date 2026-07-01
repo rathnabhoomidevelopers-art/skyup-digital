37b1575e3a4599f9fe7311d446cb906a1af17502
srinivasjsutar <sjsjasss2001@gmail.com>
2026-07-01 15:31:20 +0530

    updated the skyupinvoicetemplate

--- src/pages/ReceiptTemplate.js
+++ src/pages/ReceiptTemplate.js
@@ -100,7 +100,7 @@ export default function ReceiptTemplate({ data }) {
       </div>
 
       {/* New Table Layout with Borders */}
-      <div className="mb-5 px-4 sm:px-6 lg:px-8 h-[410px] flex items-center justify-center">
+      <div className="mb-5 px-4 sm:px-6 lg:px-8 min-h-[410px] flex items-start justify-center">
         <table
   style={{
     width: "100%",
